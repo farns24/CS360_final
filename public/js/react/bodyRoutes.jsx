b0dbe47bbbdd55b61094bf8d9c4a4d82b125e952
mafarnsworth00@gmail.com <mafarnsworth00@gmail.com>
2015-12-09 17:13:33 -0700

    Added routs for home browse

--- public/js/react/bodyRoutes.jsx
+++ public/js/react/bodyRoutes.jsx
@@ -2,12 +2,14 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
+var Popular = require('./popularList');
 
 var LoginBodyClass = require('./loginBody');
 var NewCreationClass = require('./newCreationAuth');
 var NewUserBodyClass = require('./newUserClass');
 var Base = require('./base');
-
+var Browse = require('./browse');
+var HomeBody = require('./homeBody');
 
 module.exports = (
 <Router>
@@ -15,6 +17,9 @@ module.exports = (
 	<Route path="newCreation" component={NewCreationClass}/>
 	<Route path="login" component={LoginBodyClass}/>
 	<Route path="newUser" component={NewUserBodyClass}/>
+	<Route path="popular" component={Popular}/>
+	<Route path="browse" component={Browse}/>
+	<Route path="home" component={HomeBody}/>
     </Route>
 </Router>
 )
